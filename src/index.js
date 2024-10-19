b7dd1a99895654a9e642f7996f9f15c8bd9630b8
chetan pal <cpal22387@gmail.com>
2024-10-19 12:06:07 +0530

    added db, and created some apis

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const { PORT } = require('./config/server_config');
-
+const {PORT, DB_ALTER, DB_FORCE} = require('./config/server_config');
 const ApiRouter = require('./routes/api_router');
-
+const db = require('./config/db_config');
+// const {Product,Category}=require('./models');
 
 const app = express();
 
@@ -12,6 +12,16 @@ app.use(express.text());
 
 app.use('/api', ApiRouter); // if any req comes with url starting with /api
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+    // await Category.sync();
+    // await Product.sync();
     console.log(`Server for Shopcart is Up ${PORT}`);
+    if(DB_FORCE == true) {
+        await db.sync({ force: true});
+    } else if (DB_ALTER == true) {
+        await db.sync({ alter: true});
+    } else {
+        await db.sync();
+    }
+    console.log('DB Connected');
 });
