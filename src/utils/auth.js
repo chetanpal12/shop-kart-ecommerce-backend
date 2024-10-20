1510b83191a4c4aa0d01ac03b522611b3203a248
chetan pal <cpal22387@gmail.com>
2024-10-20 17:48:20 +0530

    Added token verification method

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,8 +1,14 @@
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET, JWT_EXPIRY } = require('../config/server_config');
+
 function generateJWT(payload) {
     return jwt.sign(payload, JWT_SECRET, {expiresIn: JWT_EXPIRY})
 }
+
+function verifyToken(token) {
+    return jwt.verify(token, JWT_SECRET)
+}
+
 module.exports = {
-    generateJWT
+    generateJWT,verifyToken
 }
